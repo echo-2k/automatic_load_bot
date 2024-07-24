a0c9c26aa63850c28c07f87e823ff37ae993ee55
echo-2k <echo2k@internet.ru>
2024-07-24 09:17:47 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,13 +5,14 @@ import telegram
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-import shutil
+import pyzipper
 
 TELEGRAM_TOKEN = '123'
 CHAT_ID = '123'
 FOLDER_ID = '123'
 FILES_DIRECTORY = ''
 ARCHIVE_NAME = 'files.zip'
+ARCHIVE_PASSWORD = 'password'
 SERVICE_ACCOUNT_FILE = ''
 
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
@@ -20,12 +21,18 @@ service = build('drive', 'v3', credentials=credentials)
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
-def create_archive(directory, archive_name):
-    shutil.make_archive(archive_name.replace('.zip', ''), 'zip', directory)
+def create_archive_with_password(directory, archive_name, password):
+    with pyzipper.AESZipFile(archive_name, 'w', compression=pyzipper.ZIP_DEFLATED) as zf:
+        zf.setpassword(password.encode())
+        for foldername, subfolders, filenames in os.walk(directory):
+            for filename in filenames:
+                file_path = os.path.join(foldername, filename)
+                arcname = os.path.relpath(file_path, start=directory)
+                zf.write(file_path, arcname)
 
 def upload_files():
-    create_archive(FILES_DIRECTORY, ARCHIVE_NAME)
-    archive_path = f"{ARCHIVE_NAME}"
+    create_archive_with_password(FILES_DIRECTORY, ARCHIVE_NAME, ARCHIVE_PASSWORD)
+    archive_path = ARCHIVE_NAME
     
     file_metadata = {
         'name': ARCHIVE_NAME,
@@ -41,7 +48,7 @@ def upload_files():
 
     file_id = response.get('id')
     file_link = response.get('webViewLink')
-    bot.send_message(chat_id=CHAT_ID, text=f"File downloaded: {ARCHIVE_NAME}\nlink: {file_link}")
+    bot.send_message(chat_id=CHAT_ID, text=f"File downloaded: {ARCHIVE_NAME}\nlink: {file_link}\nPassword: {ARCHIVE_PASSWORD}")
 
 def main():
     while True:
