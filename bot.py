a80e456a0c19daa5dcc6abb72924aa6f661c4c2a
echo-2k <echo2k@internet.ru>
2024-07-25 11:24:26 +0500

    -password in archive

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ CHAT_ID = '123'
 FOLDER_ID = '123'
 FILES_DIRECTORY = ''
 ARCHIVE_NAME = 'files.zip'
-ARCHIVE_PASSWORD = 'password'
+# ARCHIVE_PASSWORD = 'password'
 SERVICE_ACCOUNT_FILE = ''
 
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
@@ -21,16 +21,29 @@ service = build('drive', 'v3', credentials=credentials)
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
-def create_archive_with_password(directory, archive_name, password):
-    with pyzipper.AESZipFile(archive_name, 'w', compression=pyzipper.ZIP_DEFLATED) as zf:
-        zf.setpassword(password.encode())
-        for foldername, subfolders, filenames in os.walk(directory):
-            for filename in filenames:
-                file_path = os.path.join(foldername, filename)
-                arcname = os.path.relpath(file_path, start=directory)
-                zf.write(file_path, arcname)
+# def create_archive_with_password(directory, archive_name, password):
+#     with pyzipper.AESZipFile(archive_name, 'w', compression=pyzipper.ZIP_DEFLATED) as zf:
+#         zf.setpassword(password.encode())
+#         for foldername, subfolders, filenames in os.walk(directory):
+#             for filename in filenames:
+#                 file_path = os.path.join(foldername, filename)
+#                 arcname = os.path.relpath(file_path, start=directory)
+#                 zf.write(file_path, arcname)
 
-def upload_files():
+def create_archive(directory, archive_name):
+    try:
+        with pyzipper.AESZipFile(archive_name, 'w', compression=pyzipper.ZIP_DEFLATED) as zf:
+            for foldername, subfolders, filenames in os.walk(directory):
+                for filename in filenames:
+                    file_path = os.path.join(foldername, filename)
+                    arcname = os.path.relpath(file_path, start=directory)
+                    print(f"Adding {file_path} as {arcname} to archive")
+                    zf.write(file_path, arcname)
+        print(f"Archive {archive_name}")
+    except Exception as e:
+        print(f"Failed: {e}")
+
+async def upload_files():
     create_archive_with_password(FILES_DIRECTORY, ARCHIVE_NAME, ARCHIVE_PASSWORD)
     archive_path = ARCHIVE_NAME
     
@@ -46,14 +59,17 @@ def upload_files():
         if status:
             print(f"Uploaded {int(status.progress() * 100)}%.")
 
-    file_id = response.get('id')
-    file_link = response.get('webViewLink')
-    bot.send_message(chat_id=CHAT_ID, text=f"File downloaded: {ARCHIVE_NAME}\nlink: {file_link}\nPassword: {ARCHIVE_PASSWORD}")
+    if response:
+        file_id = response.get('id')
+        file_link = response.get('webViewLink')
+        await bot.send_message(chat_id=CHAT_ID, text=f"File uploaded: {ARCHIVE_NAME}\nLink: {file_link}\nPassword: {ARCHIVE_PASSWORD}")
+    else:
+        print("Failed to upload file.")
 
-def main():
+async def main():
     while True:
-        upload_files()
-        time.sleep(1800)
+        await upload_files()
+        await asyncio.sleep(1800)
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
